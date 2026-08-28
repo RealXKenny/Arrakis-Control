d2e24d401710335f3704d9820b43b2e8b0b96ef0
Kenny Cline <realxkenny@gmail.com>
2026-08-28 13:58:33 -0400

    Update info.js

--- src/commands/general/info.js
+++ src/commands/general/info.js
@@ -1,4 +1,10 @@
-const { EmbedBuilder, SlashCommandBuilder, version: discordJsVersion } = require('discord.js');
+const {
+  ContainerBuilder,
+  MessageFlags,
+  SeparatorSpacingSize,
+  SlashCommandBuilder,
+  version: discordJsVersion,
+} = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,20 +15,22 @@ module.exports = {
     const client = interaction.client;
     const uptimeSeconds = Math.floor(client.uptime / 1000);
 
-    const embed = new EmbedBuilder()
-      .setColor(0x5865F2)
-      .setTitle(`${client.user.username} information`)
-      .setThumbnail(client.user.displayAvatarURL())
-      .addFields(
-        { name: 'Servers', value: String(client.guilds.cache.size), inline: true },
-        { name: 'Users cached', value: String(client.users.cache.size), inline: true },
-        { name: 'Ping', value: `${client.ws.ping}ms`, inline: true },
-        { name: 'Uptime', value: `<t:${Math.floor((Date.now() - client.uptime) / 1000)}:R> (${uptimeSeconds}s)`, inline: false },
-        { name: 'discord.js', value: `v${discordJsVersion}`, inline: true },
-        { name: 'Node.js', value: process.version, inline: true },
-      )
-      .setTimestamp();
+    const infoCard = new ContainerBuilder()
+      .setAccentColor(0x5865F2)
+      .addTextDisplayComponents((text) => text.setContent(`## ${client.user.username} information`))
+      .addSeparatorComponents((separator) => separator.setSpacing(SeparatorSpacingSize.Small))
+      .addTextDisplayComponents((text) => text.setContent([
+        `**Servers:** ${client.guilds.cache.size}`,
+        `**Users cached:** ${client.users.cache.size}`,
+        `**Ping:** ${client.ws.ping}ms`,
+        `**Uptime:** <t:${Math.floor((Date.now() - client.uptime) / 1000)}:R> (${uptimeSeconds}s)`,
+        `**discord.js:** v${discordJsVersion}`,
+        `**Node.js:** ${process.version}`,
+      ].join('\n')));
 
-    await interaction.reply({ embeds: [embed] });
+    await interaction.reply({
+      components: [infoCard],
+      flags: MessageFlags.IsComponentsV2,
+    });
   },
 };
